12043ee0cd3f848bae667d49f6919d3eeb5d3b1c
Joel <pratt.joel@gmailcom>
2016-08-24 16:23:20 -0400

    Request made with 'requests' library. Authentication verified

--- ipnd_review_getter.py
+++ ipnd_review_getter.py
@@ -1,31 +1,18 @@
-# this causes an error where are you getting this library?
-# seems you have to download the requests library separately but that doesn't seem necessary for our purposes.  
-#urllib2 should probably do the trick
-# import requests
 
-import urllib2
-import urllib
+# requests library now working
+import requests
+import json
 
 token_file = open("reviewer_api_key.txt")
 TOKEN = token_file.read()
 CERTIFICATIONS_URL = 'https://review-api.udacity.com/api/v1/me/certifications.json'
-# I'm having trouble getting through the authorization on the udacity reviews page.  
-# When I put the headers variable in my request I get an error (see below).  
-# But when when I remove them I get an 'unauthorized' error
-headers = {'Authorization': TOKEN}
-req = urllib2.Request(CERTIFICATIONS_URL, headers)
-
-response = urllib2.urlopen(req)
-page_contents = response.read()
-
-
-
-
+headers = {'Authorization': TOKEN, 'Content-Length': '0'}
+response = requests.get(CERTIFICATIONS_URL, headers=headers)
 
 def startReviewBot():
-	print TOKEN
-    print page_contents
-
+	print response.status_code
+	print response.headers['content-type']
+	print response.text
 
     
 
